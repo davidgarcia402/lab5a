f3efa3c7aa38114ba65b39da0b37a9a21a5c6ada
davidgarcia402 <davidgarcia402@gmai.com>
2024-06-17 22:13:37 -0500

    Solución main problema-1 Sum

--- js/tests.js
+++ js/tests.js
@@ -10,6 +10,9 @@ function testSum() { //eslint-disable-line
   }
 }
 
+//DRGG
+testSum();
+
 function testMultiply() { //eslint-disable-line
   if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
     console.log('%c TEST FOR multiply() PASSES', 'color: green');
