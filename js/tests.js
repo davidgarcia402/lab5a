13cb5d7f659e9624f30cbef8f5bef2316121c1ec
davidgarcia402 <davidgarcia402@gmai.com>
2024-06-18 19:25:11 -0500

    Ajuste función sum()

--- js/tests.js
+++ js/tests.js
@@ -21,6 +21,8 @@ function testMultiply() { //eslint-disable-line
   }
 }
 
+testMultiply();
+
 function testSumAndMultiply() { //eslint-disable-line
   if (sumAndMultiply(4, 7, 5)[0] === 16 && sumAndMultiply(4, 7, 5)[1] === 140 && sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
